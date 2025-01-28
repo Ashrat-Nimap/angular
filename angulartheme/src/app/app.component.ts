302e420adf184c02f3a77918372baced8f4f628d
Ashrat <ashrat.ali@nimapinfotech.com>
2025-01-28 18:46:37 +0530

    working on theme

--- angulartheme/src/app/app.component.ts
+++ angulartheme/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatInputModule,MatFormFieldModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angulartheme';
+
+
+  isDarkTheme = signal(false);
+
+  toggleTheme() {
+    this.isDarkTheme.update((current) => !current);
+
+    // Toggle theme class on the `body` element
+    const themeClass = this.isDarkTheme() ? 'dark-theme' : 'light-theme';
+    document.body.className = themeClass;
+  }
 }
